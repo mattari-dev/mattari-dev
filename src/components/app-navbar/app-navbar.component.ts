87794bbbce9ac6cc6111f508fd3a833fb93c1ec6
Jacky Tsoi <jackytsoi2015@outlook.com>
2024-01-08 22:44:52 -0800

    Create navbar component

--- src/components/app-navbar/app-navbar.component.ts
+++ src/components/app-navbar/app-navbar.component.ts
@@ -1,2 +1,13 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule],
+  templateUrl: './app-navbar.component.html',
+  styleUrls: ['./app-navbar.component.scss']
+})
+export class AppNavbarComponent {
+  // Add your component logic here
+}
