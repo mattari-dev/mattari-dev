5d6bf85d2dded1c478644af14202ca1034dc61eb
Mashed <mattari.dev@gmail.com>
2024-04-26 02:29:48 -0700

    feat: create home page

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { IconComponent } from '../icon/icon.component';
 import { VerticalDividerComponent } from '../vertical-divider/vertical-divider.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
+    RouterLink,
+    RouterLinkActive,
     IconComponent,
-    VerticalDividerComponent
+    VerticalDividerComponent,
+    TuiButtonModule,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  logo_icon = "assets/icons/logo_square.svg"
+  logo_icon = 'assets/icons/logo_square.svg';
+  discord_icon = 'assets/icons/discord.svg';
+  youtube_icon = 'assets/icons/youtube.svg';
+  github_icon = 'assets/icons/github.svg';
 }
